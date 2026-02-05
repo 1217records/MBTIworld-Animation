cf24299c900f21329658794a911e06343d1369a4
Giwoong Kim <giwoongkim@Giwoongui-Mac-Studio.local>
2026-02-06 00:13:23 +0900

    Remove legacy pages router

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./data.ts",
     "./constants.tsx",
   ],
